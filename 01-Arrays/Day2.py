3d6534588544c002cd2f4404cdf4f1b16a37e980
ayush506x <mishraji112008@gmail.com>
2026-07-07 01:27:54 +0530

    learn about INSERTION SORT

--- 01-Arrays/Day2.py
+++ 01-Arrays/Day2.py
@@ -1,4 +1,5 @@
 #learning the types of sorting how sorting works 
+'''
 # BUBBLE SORT first type of sort 
 arr = [5,3,8,1]
 n=len(arr)
@@ -17,4 +18,15 @@ for i in range(n):
         if arr[j]<arr[min_index]:
             min_index=j
     arr[i],arr[min_index] = arr[min_index],arr[i]
+print(arr)
+'''
+#INSERTION SORT third and most efficent type of sort
+arr = [5,2,4,6,1]
+for i in range(0,len(arr)):
+    key = arr[i]
+    j=i-1
+    while j>=0 and arr[j] > key:
+        arr[j+1] = arr[j]
+        j-=1
+    arr[j+1] = key
 print(arr)
