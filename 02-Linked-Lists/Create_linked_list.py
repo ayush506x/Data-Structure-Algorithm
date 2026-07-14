a44a56f9950cb316d234f9df80eeb57d9709b8f3
ayush506x <mishraji112008@gmail.com>
2026-07-14 20:28:31 +0530

    .

--- 02-Linked-Lists/Create_linked_list.py
+++ 02-Linked-Lists/Create_linked_list.py
@@ -1,10 +1,20 @@
+#Inserting in the linked list is basically easy as compare to other due to complexity 
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.next = None
-first = Node(10)
-second = Node(20)
-third = Node(30)
-first.next = second 
-second.next = third
-print(first.data)
+
+head = Node(10)
+head.next = Node(20)
+head.next.next = Node(30)
+
+new_node = Node(5)
+
+new_node.next = head
+head = new_node
+
+current = head
+
+while current:
+    print(current.data)
+    current = current.next
