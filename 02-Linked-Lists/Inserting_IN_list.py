87e94e114c9cd6ee2976cd12515ad84c9cfda59b
ayush506x <mishraji112008@gmail.com>
2026-07-16 14:57:29 +0530

    Inserting in list

--- 02-Linked-Lists/Inserting_IN_list.py
+++ 02-Linked-Lists/Inserting_IN_list.py
@@ -1,20 +1,18 @@
 #Inserting in the linked list is basically easy as compare to other due to complexity 
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+#Innserting at the Begining 
+# Using simple variables and lists instead of Node class
+head = [10, None]
+second = [20, None]
+third = [30, None]
+new_node = [40, None]
 
-head = Node(10)
-head.next = Node(20)
-head.next.next = Node(30)
+# Linking nodes
+head[1] = second
+second[1] = third
+third[1] = new_node
 
-new_node = Node(5)
-
-new_node.next = head
-head = new_node
-
+# Traverse and print
 current = head
-
-while current:
-    print(current.data)
-    current = current.next
+while current is not None:
+    print(current[0])
+    current = current[1]
