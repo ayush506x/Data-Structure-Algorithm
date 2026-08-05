a78df45ff915f35778e94bce02e7aca99a0e26b5
ayush506x <mishraji112008@gmail.com>
2026-08-05 20:01:29 +0530

    .f

--- Practice/TwoSum_01.py
+++ Practice/TwoSum_01.py
@@ -6,4 +6,20 @@ for i in range(b):
         if  a[i] + a[j] == target :
             print(i , j)
 
+       a=[int(x) for x in input().split()]
+b = len(a)
+target = int(input())
+for i in range(b):
+    for j in range(i+1,b):
+        if  a[i] + a[j] == target :
+            print(i , j)
+
+       a=[int(x) for x in input().split()]
+b = len(a)
+target = int(input())
+for i in range(b):
+    for j in range(i+1,b):
+        if  a[i] + a[j] == target :
+            print(i , j)
+
        
