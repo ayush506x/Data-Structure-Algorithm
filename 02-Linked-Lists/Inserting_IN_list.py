32323f02adb0f403bd6cd3e32e46c6941fb4557c
ayush506x <mishraji112008@gmail.com>
2026-08-09 12:59:50 +0530

    fhgf

--- 02-Linked-Lists/Inserting_IN_list.py
+++ 02-Linked-Lists/Inserting_IN_list.py
@@ -138,25 +138,5 @@ second[1] = third
 third[1] = new_node
 
 # Traverse and print
-current = head
-while current is not None:
-    print(current[0])
-    current = current[1]
-#Inserting in the linked list is basically easy as compare to other due to complexity 
-#Innserting at the Begining 
-# Using simple variables and lists instead of Node class
-head = [10, None]
-second = [20, None]
-third = [30, None]
-new_node = [40, None]
 
-# Linking nodes
-head[1] = second
-second[1] = third
-third[1] = new_node
-
-# Traverse and print
-current = head ftsd
-while current is not None:
-    print(current[0])
     current = current[1]
