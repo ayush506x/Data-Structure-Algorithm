a675859ee00211a5989e4a584a9e21954872d6c5
ayush506x <mishraji112008@gmail.com>
2026-08-16 23:24:21 +0530

    hii

--- 01-Arrays/Bubble_Sort.py
+++ 01-Arrays/Bubble_Sort.py
@@ -18,4 +18,21 @@ for i in range(n):
             arr[j],arr[j+1] = arr[j+1],arr[j]
 print(arr)
 
+sfiodkls;o#learning the types of sorting how sorting works 
+# BUBBLE SORT first type of sort 
+arr = [5,3,8,1]
+n=len(arr)
+for i in range(n):
+    for j in range(0,n-i-1):
+        if arr[j] > arr[j+1]:
+            arr[j],arr[j+1] = arr[j+1],arr[j]
+print(arr)
+
+#learning the types of sorting how sorting works 
+# BUBBLE SORT first type of sort 
+arr = [5,3,8,1]
+n=len(arr)
+for i in range(n):
+print(arr)
+
 sfiodkls;o
