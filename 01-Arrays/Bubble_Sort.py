26f99e74f744e235bed5d50fc31fe07ba5a3b893
ayush506x <mishraji112008@gmail.com>
2026-08-17 21:43:54 +0530

    ddf

--- 01-Arrays/Bubble_Sort.py
+++ 01-Arrays/Bubble_Sort.py
@@ -25,9 +25,7 @@ n=len(arr)
 for i in range(n):
     for j in range(0,n-i-1):
         if arr[j] > arr[j+1]:
-            arr[j],arr[j+1] = arr[j+1],arr[j]
-print(arr)
-
+            arr[j],arr[j+1] = arr[j+1],arr[j
 #learning the types of sorting how sorting works 
 # BUBBLE SORT first type of sort 
 arr = [5,3,8,1]
