a31e9313034b484e9f786de8d3bddcf1ef42800d
ayush506x <mishraji112008@gmail.com>
2026-09-03 14:05:17 +0530

    good

--- 01-Arrays/binary_serch.cpp
+++ 01-Arrays/binary_serch.cpp
@@ -1,33 +1,44 @@
 #include <stdio.h>
 
+int binarysearch(int a[], int beg, int end, int search) {
+    if (beg > end) {
+        return -1;  // element not found
+    }
+
+    int mid = (beg + end) / 2;
+
+    if (a[mid] == search) {
+        return mid;  // found at index mid
+    }
+    else if (a[mid] < search) {
+        return binarysearch(a, mid + 1, end, search);  // search right half
+    }
+    else {
+        return binarysearch(a, beg, mid - 1, search);  // search left half
+    }
+}
+
 int main() {
-    int a[100], n, i, serch, beg, end, mid, flag = 0;
+    int a[100], n, i, serch, result;
+
     printf("Enter size of array: ");
     scanf("%d", &n);
+
     printf("Enter elements of array (sorted):\n");
     for (i = 0; i < n; i++) {
         scanf("%d", &a[i]);
     }
+
     printf("Enter searching element: ");
     scanf("%d", &serch);
-    beg = 0;
-    end = n - 1;
-    while (beg <= end) {
-        mid = (beg + end) / 2;
-        if (a[mid] == serch) {
-            printf("Element found at index %d\n", mid);
-            flag = 1;
-            break;
-        }
-        else if (a[mid] < serch) {
-            beg = mid + 1; 
-        }
-        else {
-            end = mid - 1; 
-        }
-    }
-    if (flag == 0) {
+
+    result = binarysearch(a, 0, n - 1, serch);
+
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
         printf("Element not found\n");
     }
+
     return 0;
 }
