f157e135bd9d97134115a83e15f34e83f6b224f4
ayush506x <mishraji112008@gmail.com>
2026-09-07 09:48:00 +0530

    Succesfully Learn selectiojn sort

---
+++ 01-Arrays/selection_sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Inserting the Array:
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    // Loop is Started to check weather it is smaller than or greater:
+    for (int i = 0; i < n - 1; i++) {
+        int a = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[a]) {
+                a = j; 
+            }
+        }
+        int temp = arr[a];
+        arr[a] = arr[i];
+        arr[i] = temp;
+    }
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
